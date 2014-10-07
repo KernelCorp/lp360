bd99305fea3bb4b51f8bc0f70bec166d359f85d0
max <mk@kernel-corp.com>
2014-10-07 12:46:34 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lp360::Application.config.secret_key_base = '1bc5d49dc756a18504d589769498c417fea72e09ba55f8a75f636202dd7b71f3ab0752415efb903da4beb54fa91728cfaa2e3a7d596a85fac981cb63d23f1e33'
