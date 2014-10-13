23a8026aa58b8116d0361d1e8841802c85308146
Zamyatin-AA <stekbox@gmail.com>
2014-10-13 13:50:32 +0700

    #lp-1 Fixed tests finaly done

--- spec/models/prototype_spec.rb
+++ spec/models/prototype_spec.rb
@@ -1,16 +1,31 @@
-# require 'rails_helper'
-#
-# RSpec.describe Prototype, type: :model do
-#
-#   describe '#transfer_options' do
-#     let(:prototype) {
-#       FactoryGirl.create :prototype, products: [(FactoryGirl.create :product)],
-#                                      categories: [(FactoryGirl.create :category, products: [(FactoryGirl.create :product)])]
-#     }
-#
-#
-#     it {expect{subject}.to change{}.to(2)}
-#   end
-#
-#
-# end
+require 'rails_helper'
+
+RSpec.describe Prototype, type: :model do
+
+  describe '#transfer_options' do
+    subject{prototype.transfer_options}
+
+    let(:category) {FactoryGirl.create :category}
+    let(:product) {FactoryGirl.create :product, category: category}
+
+    context 'prototype and product have direct relation' do
+      let(:prototype) {FactoryGirl.build :prototype, products: [product], opts: ['new']}
+
+      it {expect{subject}.to change{product.options.count}.from(0).to(1) }
+    end
+
+    context 'prototype and product have relation through category' do
+      let(:prototype) {FactoryGirl.build :prototype, categories: [category], opts: ['new']}
+
+      it {expect{subject}.to change{product.options.count}.from(0).to(1) }
+    end
+
+    context 'prototype and product have no relation' do
+      let(:prototype) {FactoryGirl.build :prototype, opts: ['new']}
+
+      it {expect{subject}.to_not change{product.options.count} }
+    end
+
+  end
+
+end
