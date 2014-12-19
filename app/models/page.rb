11d8150936e32d7600185e5c98dd1a7710b422b4
max-konin <mk@kernel-corp.com>
2014-12-19 15:11:23 +0600

    fix images

--- app/models/page.rb
+++ app/models/page.rb
@@ -5,7 +5,7 @@ class Page
 
   belongs_to :parent, class_name: Page.to_s
   has_many :pages
-  embeds_many :attachments
+  embeds_many :attachments, cascade_callbacks: true
 
   accepts_nested_attributes_for :attachments
 
